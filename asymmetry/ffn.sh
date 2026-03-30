f03c13ccfc9d01559d42a81382fed2c270b4f503
t00791201 <tangtiangu@huawei.com>
2026-03-30 15:36:02 +0800

    adapt compute_gate_on_attention from True to False

--- asymmetry/ffn.sh
+++ asymmetry/ffn.sh
@@ -109,7 +109,7 @@ AFD_CONFIG='{
   "afd_extra_config": {
     "afd_size": "'$AFD_SIZE'"
   },
-  "compute_gate_on_attention": "True",
+  "compute_gate_on_attention": "False",
   "afd_port": "'"$AFD_PORT"'"
 }'
 echo "AFD_CONFIG:$AFD_CONFIG"
@@ -126,6 +126,7 @@ echo "EXPERT_PER_RANK:$EXPERT_PER_RANK"
 #     "enable_force_load_balance": "True",
 #     "force_load_balance_topn_per_rank": '"$EXPERT_PER_RANK"'
 #  }" \
+# "force_load_balance_topn_per_rank": '"$EXPERT_PER_RANK"'
 python -m vllm.entrypoints.afd_ffn_server "$MODEL_PATH" \
     --tensor-parallel-size $NUM_DEVICES \
     --enable-expert-parallel \
@@ -141,8 +142,7 @@ python -m vllm.entrypoints.afd_ffn_server "$MODEL_PATH" \
     --max-model-len $MAX_MODEL_LEN \
     --afd-config "$AFD_CONFIG" \
     --additional-config '{
-        "enable_force_load_balance": "True",
-        "force_load_balance_topn_per_rank": '"$EXPERT_PER_RANK"'
+        "enable_force_load_balance": "True"
     }' \
     --kv-transfer-config '{
         "kv_connector": "DecodeBenchConnector",
